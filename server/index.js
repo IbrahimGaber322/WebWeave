bb7e25cccea4caf902efdb46371e16f98a6880a8
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-05-07 05:16:37 +0300

    fontend url

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ io.listen(SOCKET_PORT, () => {
 app.use(limiter);
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use(cors({ origin: FRONTEND_URL }));
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
