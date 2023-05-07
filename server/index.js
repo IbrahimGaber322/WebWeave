459760fc3869d7a5697d3ab12d5fc89223d19422
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-05-07 05:32:32 +0300

    change cors order

--- server/index.js
+++ server/index.js
@@ -45,10 +45,11 @@ io.listen(SOCKET_PORT, () => {
     console.log(`Server listening on port ${SOCKET_PORT}`);
   });
 
+app.use(cors({ origin: FRONTEND_URL })); 
 app.use(limiter);
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors({ origin: FRONTEND_URL }));
+
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
